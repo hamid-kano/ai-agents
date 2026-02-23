8fc43c1574eae2f5b691b4134696b86c606ad608
hk-opead <hko-1992@hotmail.com>
2026-02-23 11:38:59 +0300

    لإ

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,7 +9,7 @@
         
         <div class="p-8">
             <div class="prose prose-invert max-w-none mb-6 prose-h1:text-4xl prose-h1:font-bold prose-h1:bg-gradient-to-r prose-h1:from-cyan-300 prose-h1:via-purple-300 prose-h1:to-pink-300 prose-h1:bg-clip-text prose-h1:text-transparent prose-strong:text-cyan-300 prose-strong:font-bold prose-p:text-gray-200 prose-p:mb-0">
-                <h1 class="text-4xl font-bold bg-gradient-to-r from-cyan-300 via-purple-300 to-pink-300 bg-clip-text text-transparent">{!! nl2br(e($article->topic)) !!}</h1>
+                <h1 class="text-4xl font-bold bg-gradient-to-r from-cyan-300 via-purple-300 to-pink-300 bg-clip-text text-transparent">{!! nl2br(e(str_replace('**', '', $article->topic))) !!}</h1>
             </div>
 
             @if($article->seo_data)
@@ -24,7 +24,7 @@
             @endif
 
             <div class="text-white text-lg leading-relaxed space-y-6">
-                {!! nl2br(e($article->content)) !!}
+                {!! nl2br(e(str_replace('**', '', $article->content))) !!}
             </div>
 
             <div class="mt-8 pt-6 border-t border-purple-500/20">
